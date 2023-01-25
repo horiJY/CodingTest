c0c484da2fbb432b82971e2667eed83e9b7e2b64
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-01-25 22:20:33 +0900

    [Silver IV] Title: 설탕 배달, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -7,25 +7,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int answer = Integer.MAX_VALUE;
-        for (int i = 0; i * 5 <= N; i++) {
-            answer = Math.min(answer, getDivision(N, i));
+        for (int five = N / 5; five >= 0; five--) {
+            int three = (N - (five * 5)) / 3;
+            if (five * 5 + three * 3 == N) {
+                System.out.println(five + three);
+                return;
+            }
         }
 
-        if (answer == Integer.MAX_VALUE) {
-            System.out.println(-1);
-        } else {
-            System.out.println(answer);
-        }
-    }
-
-    private static int getDivision(int n, int i) {
-        n -= 5 * i;
-        int result = n / 3;
-        n -= result * 3;
-        if (n == 0) {
-            return result + i;
-        }
-        return Integer.MAX_VALUE;
+        System.out.println(-1);
     }
 }
