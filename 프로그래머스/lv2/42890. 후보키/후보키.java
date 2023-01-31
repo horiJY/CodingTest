4f7f09fcf4d6273fc788955c3f4ff52817a8ee35
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-01-31 14:31:13 +0900

    [level 2] Title: 후보키, Time: 12.29 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42890. 후보키/후보키.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+class Solution {
+    static ArrayList<String> candidateKeyList;
+
+    static public int solution(String[][] relation) {
+        candidateKeyList = new ArrayList<>();
+        for (int i = 1; i <= relation[0].length; i++) {
+            combineCandidateKey(relation, i, 0, "");
+        }
+
+        return candidateKeyList.size();
+    }
+
+    static private void combineCandidateKey(String[][] relation, int combineKeyLimit, int startIdx, String key) {
+        if (key.length() == combineKeyLimit) {
+
+            if (validCandidateKey(relation, key)) {
+                candidateKeyList.add(key);
+            }
+            return;
+        }
+
+        for (int i = startIdx; i < relation[0].length; i++) {
+            if (!key.contains(String.valueOf(i))) {
+                combineCandidateKey(relation, combineKeyLimit, i + 1, key + String.valueOf(i));
+            }
+        }
+    }
+
+    private static boolean validCandidateKey(String[][] relation, String key) {
+        // 최소성 검사
+        for (String candidateKey : candidateKeyList) {
+            boolean[] usedCol = new boolean[candidateKey.length()];
+            for (int i = 0; i < usedCol.length; i++) {
+                for (char c : key.toCharArray()) {
+                    if (candidateKey.charAt(i) == c) {
+                        usedCol[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            boolean valid = false;
+            for (boolean use : usedCol) {
+                if (!use) {
+                    valid = true;
+                }
+            }
+            if (!valid) {
+                return false;
+            }
+        }
+        // 유효성 검사
+        HashSet<String> tempSet = new HashSet<>();
+        for (String[] data : relation) {
+            String tempData = "";
+            for (String s : key.split("")) {
+                tempData += data[Integer.parseInt(s)];
+            }
+            tempSet.add(tempData);
+        }
+        if (tempSet.size() == relation.length) {
+            return true;
+        }
+        return false;
+
+    }
+}
