f6ddb8ff9c9ebd79706fc82287fc15079d1e5d42
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-02-01 01:43:05 +0900

    [level 2] Title: 후보키, Time: 9.20 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.java
+++ 프로그래머스/lv2/42890. 후보키/후보키.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 class Solution {
@@ -7,6 +8,7 @@ class Solution {
     static public int solution(String[][] relation) {
         candidateKeyList = new ArrayList<>();
         for (int i = 1; i <= relation[0].length; i++) {
+            // i = 1 -> 2
             combineCandidateKey(relation, i, 0, "");
         }
 
@@ -14,8 +16,8 @@ class Solution {
     }
 
     static private void combineCandidateKey(String[][] relation, int combineKeyLimit, int startIdx, String key) {
+        // combineKeyLimit = 2, startIdx = 1, key = "01"
         if (key.length() == combineKeyLimit) {
-
             if (validCandidateKey(relation, key)) {
                 candidateKeyList.add(key);
             }
@@ -23,6 +25,7 @@ class Solution {
         }
 
         for (int i = startIdx; i < relation[0].length; i++) {
+
             if (!key.contains(String.valueOf(i))) {
                 combineCandidateKey(relation, combineKeyLimit, i + 1, key + String.valueOf(i));
             }
@@ -32,38 +35,27 @@ class Solution {
     private static boolean validCandidateKey(String[][] relation, String key) {
         // 최소성 검사
         for (String candidateKey : candidateKeyList) {
-            boolean[] usedCol = new boolean[candidateKey.length()];
-            for (int i = 0; i < usedCol.length; i++) {
-                for (char c : key.toCharArray()) {
-                    if (candidateKey.charAt(i) == c) {
-                        usedCol[i] = true;
-                        break;
-                    }
-                }
-            }
-
-            boolean valid = false;
-            for (boolean use : usedCol) {
-                if (!use) {
-                    valid = true;
-                }
-            }
-            if (!valid) {
+            HashSet<String> candidateKeySet = new HashSet<>(Arrays.asList(candidateKey.split("")));
+            candidateKeySet.removeAll(new HashSet<>(Arrays.asList(key.split(""))));
+            if (candidateKeySet.size() == 0) {
                 return false;
             }
         }
-        // 유효성 검사
+
+        // 유일성 검사
         HashSet<String> tempSet = new HashSet<>();
         for (String[] data : relation) {
-            String tempData = "";
+            String tempKey = "";
             for (String s : key.split("")) {
-                tempData += data[Integer.parseInt(s)];
+                tempKey += data[Integer.parseInt(s)];
             }
-            tempSet.add(tempData);
+            tempSet.add(tempKey);
         }
+
         if (tempSet.size() == relation.length) {
             return true;
         }
+
         return false;
 
     }
