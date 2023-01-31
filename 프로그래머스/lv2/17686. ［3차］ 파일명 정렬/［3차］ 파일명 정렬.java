f7ef105357aaa867cf7e7f38bc38c64af705f92a
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-01-31 12:30:19 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 25.01 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Solution {
+    private static final Pattern GET_NUMBER = Pattern.compile("[0-9]+");
+
+    public String[] solution(String[] files) {
+        ArrayList<String[]> fileList = new ArrayList<>();
+        for (String file : files) {
+            String[] data = file.split("[\\d]");
+            String[] temp = new String[] { file, data[0].toLowerCase(), "" };
+
+            Matcher matcher = GET_NUMBER.matcher(file);
+            if (matcher.find()) {
+                temp[2] = matcher.group();
+            }
+            fileList.add(temp);
+        }
+
+        fileList.sort(new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                if (o1[1].equals(o2[1])) {
+                    return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
+                }
+                return o1[1].compareTo(o2[1]);
+            }
+        });
+
+        return fileList.stream().map(e -> e[0]).toArray(String[]::new);
+    }
+}
