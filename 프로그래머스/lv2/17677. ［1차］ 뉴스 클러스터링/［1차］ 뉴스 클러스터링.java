22fa7eda9c0271fcc92745067f822c9e716d92b9
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-01-31 11:34:43 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.65 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+class Solution {
+    public int solution(String str1, String str2) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        if (str1.equals(str2)) {
+            return 65536;
+        }
+
+        HashMap<String, Integer> str1Group = new HashMap<>();
+        HashMap<String, Integer> str2Group = new HashMap<>();
+
+        for (int i = 2; i <= str1.length(); i++) {
+            String target = str1.substring(i - 2, i);
+            if (target.matches("[a-z]{2}")) {
+                str1Group.put(target, str1Group.getOrDefault(target, 0) + 1);
+            }
+        }
+
+        for (int i = 2; i <= str2.length(); i++) {
+            String target = str2.substring(i - 2, i);
+            if (target.matches("[a-z]{2}")) {
+                str2Group.put(target, str2Group.getOrDefault(target, 0) + 1);
+            }
+        }
+
+        HashMap<String, Integer> unionGroup = new HashMap<>();
+        int union = 0;
+        int intersection = 0;
+        for (Entry<String, Integer> str1Entry : str1Group.entrySet()) {
+            unionGroup.put(str1Entry.getKey(), str1Entry.getValue());
+            boolean intersectionFlag = false;
+            for (Entry<String, Integer> str2Entry : str2Group.entrySet()) {
+                if (str1Entry.getKey().equals(str2Entry.getKey())) {
+                    intersectionFlag = true;
+                    intersection += Math.min(str1Entry.getValue(), str2Entry.getValue());
+                    union += Math.max(str1Entry.getValue(), str2Entry.getValue());
+                    break;
+                }
+            }
+            if (!intersectionFlag) {
+                union += str1Entry.getValue();
+            }
+        }
+        for (Entry<String, Integer> str2Entry : str2Group.entrySet()) {
+            if (unionGroup.get(str2Entry.getKey()) == null) {
+                union += str2Entry.getValue();
+            }
+        }
+
+        double answer = Double.valueOf(intersection) / Double.valueOf(union) * 65536;
+        return (int) Math.floor(answer);
+    }
+}
