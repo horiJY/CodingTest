54ebf6fd2741e852b6e14e8474e8c5fc53761f8b
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-02-08 03:47:28 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -14,6 +14,11 @@ class Solution {
                 }
             }
 
+            if (skill.length() == 1 && skillSequence[0] == Integer.MAX_VALUE) {
+                answer++;
+                continue;
+            }
+
             answer++;
             for (int i = 1; i < skillSequence.length; i++) {
                 if (skillSequence[i - 1] > skillSequence[i]) {
