776646512273a345eb7790e73595d907a5321002
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-02-08 03:37:30 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        int[] skillSequence = new int[skill.length()];
+
+        for (String tree : skill_trees) {
+            Arrays.fill(skillSequence, Integer.MAX_VALUE);
+
+            for (int i = 0; i < skillSequence.length; i++) {
+                if (tree.indexOf(skill.charAt(i)) != -1) {
+                    skillSequence[i] = tree.indexOf(skill.charAt(i));
+                }
+            }
+
+            answer++;
+            for (int i = 1; i < skillSequence.length; i++) {
+                if (skillSequence[i - 1] > skillSequence[i]) {
+                    answer--;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
