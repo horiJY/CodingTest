89293c814cfa5079f430b1a46d22b4d2e81065ae
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-03-27 22:49:41 +0900

    [unrated] Title: 디펜스 게임, Time: 5.52 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    private static int dn, dk;
+    private static int[] enemyClone;
+
+    public static int solution(int n, int k, int[] enemy) {
+        dn = n;
+        dk = k;
+        enemyClone = enemy;
+
+        return binarySearch();
+    }
+
+    private static int binarySearch() {
+        int left = 0;
+        int right = enemyClone.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            // mid 라운드 까지 막을 수 있는지 확인
+            if (validDefence(mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    private static boolean validDefence(int mid) {
+        List<Integer> collect = Arrays.stream(enemyClone, 0, mid + 1)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
+        int n = dn;
+        int size = collect.size();
+
+        for (int i = 0; i < size; i++) {
+            Integer val = collect.get(i);
+            if (n >= val) {
+                n -= val;
+                continue;
+            }
+            // 남은 라운드를 무적권을 통해 skip
+            return dk >= size - i;
+        }
+        return true;
+    }
+}
