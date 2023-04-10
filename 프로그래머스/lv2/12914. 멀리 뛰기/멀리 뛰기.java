8606ab54b20b9e7b45399335ecb165e5a2f22e5e
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-04-11 00:32:04 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -3,7 +3,6 @@ class Solution {
         long[] dp = new long[2001];
         dp[0] = 1;
         dp[1] = 1;
-        // dp[2] = 2;
         for (int i = 2; i <= n; i++) {
             dp[i] += (dp[i - 1] + dp[i - 2]) % 1234567;
         }
