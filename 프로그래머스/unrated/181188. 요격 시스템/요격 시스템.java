af4f3aa8a478083f8fc97c2d0cce0dcff6907edb
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-04-19 21:19:55 +0900

    [unrated] Title: 요격 시스템, Time: 0.44 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -5,7 +5,9 @@ class Solution {
         int mark = 0;
         int answer = 0;
         Arrays.sort(targets, (o1, o2) -> {
-            
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
             return o1[1] - o2[1];
         });
         for (int i = 1; i < targets.length; i++) {
