68b273bad5a2b2b1b4f2cec84dc9a61a7a5608c3
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-05-07 19:34:14 +0900

    [unrated] Title: 당구 연습, Time: 1.59 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/169198. 당구 연습/당구 연습.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = new int[balls.length];
+        int[] start = new int[] { startX, startY };
+
+        // x축과 y축 중 어느쪽으로 맞아야 더 짧은가?
+        for (int i = 0; i < answer.length; i++) {
+            int minDistance = Integer.MAX_VALUE;
+            List<int[]> tempTarget = getSymmetricTranspositionWithBall(m, n, start, balls[i]);
+            for (int[] target : tempTarget) {
+                minDistance = Math.min(minDistance, getShortRouteWithTowPoint(start, target));
+            }
+            answer[i] = minDistance;
+        }
+
+        return answer;
+    }
+
+    private static List<int[]> getSymmetricTranspositionWithBall(int m, int n, int[] start, int[] ball) {
+        List<int[]> result = new ArrayList<>();
+        /**
+         * 튕길 수 있는 4가지 조건
+         * - 아래 쿠션
+         * - 위 쿠션
+         * - 오른쪽 쿠션
+         * - 왼쪽 쿠션
+         */
+        if (!(start[0] == ball[0] && start[1] > ball[1])) { // 아래 쿠션
+            result.add(new int[] { ball[0], ball[1] * -1 });
+        }
+        if (!(start[0] == ball[0] && start[1] < ball[1])) { // 위 쿠션
+            result.add(new int[] { ball[0], n + (n - ball[1]) });
+        }
+        if (!(start[1] == ball[1] && start[0] < ball[0])) { // 오른 쿠션
+            result.add(new int[] { m + (m - ball[0]), ball[1] });
+        }
+        if (!(start[1] == ball[1] && start[0] > ball[0])) { // 왼 쿠션
+            result.add(new int[] { ball[0] * -1, ball[1] });
+        }
+
+        return result;
+    }
+
+    private int getShortRouteWithTowPoint(int[] start, int[] target) {
+        int maxX = Math.max(start[0], target[0]);
+        int minX = Math.min(start[0], target[0]);
+        int maxY = Math.max(start[1], target[1]);
+        int minY = Math.min(start[1], target[1]);
+        return (int) Math.pow(maxX - minX, 2) + (int) Math.pow(maxY - minY, 2);
+    }
+}
