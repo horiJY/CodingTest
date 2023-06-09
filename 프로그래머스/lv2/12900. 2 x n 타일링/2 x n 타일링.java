4c7ee2d4a7b69549d6475e8e4dd7dfe9a27c7f56
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-06-09 16:16:34 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.42 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,10 +1,6 @@
 class Solution {
-    // dp[i] = 가로 길이갸 i 일때 타일 배치 경우의수
-    // dp[i] = dp[i-1] + dp[i-2]
-    // dp[i] = i-1번째에는 타일 세로 배치만 가능, i-2 번째에는 타일 가로 배치만 가능
-    private static int[] dp;
-
     public static int solution(int n) {
+        int[] dp;
         dp = new int[60_001];
         dp[1] = 1;
         dp[2] = 2;
