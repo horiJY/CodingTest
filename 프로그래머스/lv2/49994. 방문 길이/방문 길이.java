8946c88677c4fbe3e0f19c795053350e6513a796
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-06-27 10:52:44 +0900

    [level 2] Title: 방문 길이, Time: 18.20 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    static private int positionLimit = 5;
+
+    public int solution(String dirs) {
+        int[] cur = new int[] { 0, 0 };
+        HashMap<Character, int[]> move = new HashMap<>();
+        move.put('U', new int[] { 0, 1 });
+        move.put('D', new int[] { 0, -1 });
+        move.put('L', new int[] { -1, 0 });
+        move.put('R', new int[] { 1, 0 });
+        Set<String> visit = new HashSet<>();
+        int answer = 0;
+        for (char next : dirs.toCharArray()) {
+            int[] dxdy = move.get(next);
+            if (isValid(cur, dxdy)) {
+                String path = "" + cur[0] + cur[1] + dxdy[0] + dxdy[1];
+                cur[0] += dxdy[0];
+                cur[1] += dxdy[1];
+                if (!visit.contains(path)) {
+                    answer++;
+                    visit.add(path);
+                    visit.add("" + cur[0] + cur[1] + (dxdy[0] * -1) + (dxdy[1] * -1));
+                }
+            }
+        }
+        return answer;
+    }
+
+    private boolean isValid(int[] cur, int[] dxdy) {
+        if (Math.abs(cur[0] + dxdy[0]) <= positionLimit && Math.abs(cur[1] + dxdy[1]) <= positionLimit) {
+            return true;
+        }
+        return false;
+    }
+}
