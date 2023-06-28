734c7420a6f1a7e40c01b3024a3d4bf344a92963
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-06-28 17:24:40 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 35.02 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int[] solution(long[] numbers) {
+        int[] answer = new int[numbers.length];
+        for (int i = 0; i < answer.length; i++) {
+            if (numbers[i] == 0) {
+                answer[i] = 1;
+            }
+            String numberBinaryStr = getFullBinaryStrLength(Long.toBinaryString(numbers[i]));
+            if (isBinaryTree(numberBinaryStr)) {
+                answer[i] = 1;
+            } else {
+                answer[i] = 0;
+            }
+        }
+        return answer;
+    }
+
+    private String getFullBinaryStrLength(String binaryStr) {
+        int binaryStrLen = binaryStr.length();
+        int fullLength = 1;
+        int level = 1;
+        while (binaryStrLen > fullLength) {
+            level *= 2;
+            fullLength += level;
+        }
+
+        int offset = fullLength - binaryStrLen;
+        return "0".repeat(offset) + binaryStr;
+    }
+
+    private boolean isBinaryTree(String binaryStr) {
+        int len = binaryStr.length();
+
+        if (binaryStr.length() == 0) {
+            return true;
+        }
+
+        int root = len / 2;
+        String leftSubStr = binaryStr.substring(0, root);
+        String rightSubStr = binaryStr.substring(root + 1);
+
+        if (binaryStr.charAt(root) == '0') {
+            return isZeroTree(leftSubStr) && isZeroTree(rightSubStr);
+        }
+
+        return isBinaryTree(leftSubStr) && isBinaryTree(rightSubStr);
+    }
+
+    private boolean isZeroTree(String binaryStr) {
+        int len = binaryStr.length();
+        if (binaryStr.length() == 0) {
+            return true;
+        }
+
+        int root = len / 2;
+        String leftSubStr = binaryStr.substring(0, root);
+        String rightSubStr = binaryStr.substring(root + 1);
+
+        if (binaryStr.charAt(root) == '1') {
+            return false;
+        }
+
+        return isZeroTree(leftSubStr) && isZeroTree(rightSubStr);
+    }
+}
