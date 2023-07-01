6143a55629c251702f41ec4ab47e6af37aabd000
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-07-02 01:16:36 +0900

    [level 3] Title: 야근 지수, Time: 269.96 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -4,8 +4,8 @@ import java.util.PriorityQueue;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-
         PriorityQueue<Long> pQue = new PriorityQueue<>(Collections.reverseOrder());
+
         for (int i = 0; i < works.length; i++) {
             pQue.offer((long) works[i]);
         }
@@ -14,16 +14,13 @@ class Solution {
             return 0;
         }
 
-        long maxTime = 0L;
         while (n > 0) {
-            maxTime = pQue.poll();
-            maxTime--;
-            pQue.add(maxTime);
+            pQue.add(pQue.poll() - 1);
             n--;
         }
 
-        if (!pQue.isEmpty()) {
-            answer = pQue.stream().reduce(0L, (a, b) -> a + Math.round(Math.pow(b, 2)));
+        while (!pQue.isEmpty()) {
+            answer += Math.pow(pQue.poll(), 2);
         }
 
         return answer;
