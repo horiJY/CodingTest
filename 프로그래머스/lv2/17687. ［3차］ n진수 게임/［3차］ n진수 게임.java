7d38bbe8ae77fc9146770e351aa4a87e4a8c0a4f
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-07-01 18:37:57 +0900

    [level 2] Title: [3차] n진수 게임, Time: 4.68 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,10 +1,10 @@
-
 class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder answer = new StringBuilder();
         int idx = p;
         for (int number = 0; answer.length() < t; number++) {
-            String numberStr = Integer.toString(number, n).toUpperCase();
+            // String numberStr = Integer.toString(number,n).toUpperCase();
+            String numberStr = getNumberStringWithNotation(number, n);
 
             int remainderCnt = numberStr.length() % m;
             while (idx - 1 < numberStr.length()) {
@@ -17,5 +17,31 @@ class Solution {
             idx = (idx - remainderCnt) % m == 0 ? m : (idx - remainderCnt) % m;
         }
         return answer.toString();
+    }
+
+    private String getNumberStringWithNotation(int number, int notation) {
+        StringBuilder sb = new StringBuilder();
+        while (number >= 0) {
+            if (number < notation) {
+                sb.insert(0, parseStringWithNumber(number));
+                break;
+            }
+            sb.insert(0, parseStringWithNumber(number % notation));
+
+            number /= notation;
+        }
+        return sb.toString();
+    }
+
+    private String parseStringWithNumber(int number) {
+        return switch (number) {
+            case 10 -> "A";
+            case 11 -> "B";
+            case 12 -> "C";
+            case 13 -> "D";
+            case 14 -> "E";
+            case 15 -> "F";
+            default -> String.valueOf(number);
+        };
     }
 }
