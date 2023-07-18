40d62a68d3d63fbce89088f901d9c4c708b24850
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-07-18 23:47:27 +0900

    [level 4] Title: 도둑질, Time: 23.77 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.java
@@ -0,0 +1,30 @@
+class Solution {
+    static int[] memo;
+
+    public int solution(int money[]) {
+        if (money.length == 1) {
+            return money[0];
+        }
+
+        int answer = 0;
+        // 첫번째 선택
+        memo = new int[money.length];
+        memo[0] = money[0];
+        memo[1] = money[0];
+        for (int i = 2; i < money.length - 1; i++) {
+            memo[i] = Math.max(memo[i - 1], memo[i - 2] + money[i]);
+        }
+        answer = Math.max(memo[memo.length - 1], memo[memo.length - 2]);
+
+        // 두번째 선택
+        memo = new int[money.length];
+        memo[0] = 0;
+        memo[1] = money[1];
+        for (int i = 2; i < money.length; i++) {
+            memo[i] = Math.max(memo[i - 1], memo[i - 2] + money[i]);
+        }
+        answer = Math.max(answer, Math.max(memo[memo.length - 1], memo[memo.length - 2]));
+
+        return answer;
+    }
+}
