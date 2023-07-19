ee2b82c7688c05edd37a65d9e2791c9e5df750bd
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-07-19 16:23:51 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.02 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,30 +1,30 @@
 class Solution {
-    static int[] memo;
+    public int solution(int[] sticker) {
+        int length = sticker.length;
 
-    public int solution(int sticker[]) {
-        if (sticker.length == 1) {
+        if (length == 1) {
             return sticker[0];
         }
 
-        int answer = 0;
-        // 첫번째 스티커 선택
-        memo = new int[sticker.length];
-        memo[0] = sticker[0];
-        memo[1] = sticker[0];
-        for (int i = 2; i < sticker.length - 1; i++) {
-            memo[i] = Math.max(memo[i - 1], memo[i - 2] + sticker[i]);
-        }
-        answer = Math.max(memo[memo.length - 1], memo[memo.length - 2]);
+        int[] dp1 = new int[length]; // 첫 번째 스티커를 뜯는 경우
+        int[] dp2 = new int[length]; // 첫 번째 스티커를 뜯지 않는 경우
 
-        // 두번째 스티커 선택
-        memo = new int[sticker.length];
-        memo[0] = 0;
-        memo[1] = sticker[1];
-        for (int i = 2; i < sticker.length; i++) {
-            memo[i] = Math.max(memo[i - 1], memo[i - 2] + sticker[i]);
-        }
-        answer = Math.max(answer, Math.max(memo[memo.length - 1], memo[memo.length - 2]));
+        // 첫 번째 스티커를 뜯는 경우
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[0];
 
-        return answer;
+        for (int i = 2; i < length - 1; i++) {
+            dp1[i] = Math.max(dp1[i - 2] + sticker[i], dp1[i - 1]);
+        }
+
+        // 첫 번째 스티커를 뜯지 않는 경우
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+
+        for (int i = 2; i < length; i++) {
+            dp2[i] = Math.max(dp2[i - 2] + sticker[i], dp2[i - 1]);
+        }
+
+        return Math.max(dp1[length - 2], dp2[length - 1]);
     }
 }
