5c11bba60ac9c60564fa06ac0474198818470801
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-07-25 01:28:57 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 552 ms, Memory: 48568 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,91 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static final int[][] moving = new int[][] { { 0, -1 }, { -1, 0 }, { 1, 0 }, { 0, 1 }, };
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] answer = new int[N][M];
+		Queue<int[]> queue = new ArrayDeque<>();
+		for(int i=0;i<N;i++){
+			st = new StringTokenizer(in.readLine());
+			for(int j=0;j<M;j++){
+				int num = Integer.parseInt(st.nextToken());
+				if(num!=1) answer[i][j] = 0;
+				else answer[i][j] = -1;
+				if(num==2) queue.offer(new int[]{i,j});
+			}
+		}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
-        Point start = new Point(0, 0);
-        int[][] map = new int[N][M];
-        int[][] answer = new int[N][M];
-        for (int y = 0; y < N; y++) {
-            input = br.readLine().split(" ");
-            for (int x = 0; x < M; x++) {
-                int e = Integer.parseInt(input[x]);
-                if (e == 2) {
-                    start.y = y;
-                    start.x = x;
-                }
-                map[y][x] = Integer.parseInt(input[x]);
-            }
-        }
+		int[][] deltas = {{1,0},{-1,0},{0,1},{0,-1}};
+		int[] node;
+		int ni,nj;
+		while(!queue.isEmpty()){
+			node = queue.poll();
+			for(int[] d:deltas){
+				ni = node[0]+d[0];
+				nj = node[1]+d[1];
+				if(ni<0||nj<0||ni>=N||nj>=M||answer[ni][nj]!=-1) continue;
+				answer[ni][nj] = answer[node[0]][node[1]]+1;
+				queue.offer(new int[]{ni,nj});
+			}
+		}
 
-        Deque<Point> que = new ArrayDeque<>();
-        que.add(start);
-        while (!que.isEmpty()) {
-            Point cur = que.poll();
-            for (int i = 0; i < moving.length; i++) {
-                int dy = cur.y + moving[i][0];
-                int dx = cur.x + moving[i][1];
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<N;i++){
+			for(int j=0;j<M;j++){
+				sb.append(answer[i][j]).append(' ');
+			}
+			sb.append('\n');
+		}
 
-                if (dy < 0 || dy >= N || dx < 0 || dx >= M) {
-                    continue;
-                }
-
-                if (map[dy][dx] == 1 && answer[dy][dx] == 0) {
-                    answer[dy][dx] = answer[cur.y][cur.x] + 1;
-                    que.add(new Point(dy, dx));
-                }
-            }
-        }
-
-        for (int y = 0; y < N; y++) {
-            for (int x = 0; x < M; x++) {
-                if (!(y == start.y && x == start.x) && map[y][x] != 0 && answer[y][x] == 0) {
-                    boolean isReachable = false;
-                    for (int i = 0; i < moving.length; i++) {
-                        int dy = y + moving[i][0];
-                        int dx = x + moving[i][1];
-                        if (dy < 0 || dy >= N || dx < 0 || dx >= M) {
-                            continue;
-                        }
-                        if (answer[dy][dx] > 0) {
-                            isReachable = true;
-                            break;
-                        }
-                    }
-                    if (!isReachable) {
-                        answer[y][x] = -1;
-                    }
-                }
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int[] y : answer) {
-            for (int i : y) {
-                sb.append(i).append(' ');
-            }
-            sb.append("\n");
-        }
-
-        System.out.println(sb);
-    }
-
-    public static class Point {
-        public Point(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-
-        int y;
-        int x;
-    }
+		System.out.print(sb);
+	}
 }
