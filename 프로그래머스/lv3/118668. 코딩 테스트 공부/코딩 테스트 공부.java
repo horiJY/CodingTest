be5c7502d595a2866952826f58d9a36739af707d
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-08-23 12:17:58 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 14.60 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int alp, int cop, int[][] problems) {
+        int targetAlp = alp;
+        int targetCop = cop;
+        for (int[] is : problems) {
+            targetAlp = Math.max(targetAlp, is[0]);
+            targetCop = Math.max(targetCop, is[1]);
+        }
+
+        int[][] memo = new int[targetAlp + 20][targetCop + 20];
+        for (int i = 0; i < memo.length; i++) {
+            Arrays.fill(memo[i], Integer.MAX_VALUE);
+        }
+        memo[alp][cop] = 0;
+
+        for (int cur_alp = alp; cur_alp < memo.length - 1; cur_alp++) {
+            for (int cur_cop = cop; cur_cop < memo[0].length - 1; cur_cop++) {
+                memo[cur_alp + 1][cur_cop] = Math.min(memo[cur_alp + 1][cur_cop], memo[cur_alp][cur_cop] + 1);
+                memo[cur_alp][cur_cop + 1] = Math.min(memo[cur_alp][cur_cop + 1], memo[cur_alp][cur_cop] + 1);
+                for (int[] problem : problems) {
+                    if (cur_alp >= problem[0] && cur_cop >= problem[1]) {
+                        int nextAlp = cur_alp + problem[2] > targetAlp ? targetAlp : cur_alp + problem[2];
+                        int nextCop = cur_cop + problem[3] > targetCop ? targetCop : cur_cop + problem[3];
+
+                        memo[nextAlp][nextCop] = Math.min(memo[nextAlp][nextCop],
+                                memo[cur_alp][cur_cop] + problem[4]);
+                    }
+
+                }
+
+            }
+        }
+
+        return memo[targetAlp][targetCop];
+    }
+}
