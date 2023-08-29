593833fd3625659403ff71231ce41a0c56877d38
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-08-29 21:06:19 +0900

    [unrated] Title: 억억단을 외우자, Time: 1715.13 ms, Memory: 432 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138475. 억억단을 외우자/억억단을 외우자.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int e, int[] starts) {
+        int[] numAppearCount = new int[e + 1];
+        for (int i = 1; i <= e; i++) {
+            for (int j = i; j <= e; j += i) {
+                numAppearCount[j]++;
+            }
+        }
+        
+        int[][] numAppearSort = new int[e][2];
+        for (int i = 1; i <= e; i++) {
+            numAppearSort[i - 1] = new int[] { i, numAppearCount[i] };
+        }
+        
+        Arrays.sort(numAppearSort, (o1, o2) -> o2[1] - o1[1]);
+        
+        // starts[i]<= answer[i] <= e 범위수 중 많이 등장한 수목록에서 제일 작은 값
+        int[] answer = new int[starts.length];
+        for (int i = 0; i < starts.length; i++) {
+            for (int j = 0; j < numAppearSort.length; j++) {
+                if (numAppearSort[j][0] >= starts[i]) {
+                    answer[i] = numAppearSort[j][0];
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
