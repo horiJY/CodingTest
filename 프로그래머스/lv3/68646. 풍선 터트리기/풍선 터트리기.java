e7045d13a629af62971a3df29e137cf172a049ba
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-08-30 20:54:54 +0900

    [level 3] Title: 풍선 터트리기, Time: 228.71 ms, Memory: 200 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -7,6 +7,10 @@ class Solution {
     // 따라서, i를 기준으로 오른쪽과 왼쪽에서 제일 작은 값을 가져와 세가지를 비교했을 때 i가 제일 작거나 2번째로 작으면 된다.
     public int solution(int[] a) {
         Set<Integer> answer = new HashSet<>();
+        // i위치에서 왼쪽의 최소값(0~i-1)과 오른쪽의 최소값(i+1 ~ a.length)까지 값을 매번 찾으려면 시간 초과
+        // 1 <= a.length <= 1_000_000
+        // 왼쪽에서 i까지 최소값을 저장하는 leftMinNumberCache
+        // 오른쪽에서 i까지 최소값을 저장하는 rightMinNumberCache
         int[] leftMinNumberCache = new int[a.length];
         int[] rightMinNumberCache = new int[a.length];
         leftMinNumberCache[0] = a[0];
