c05af60abfb875883081b9c5a323ec1b546d88d6
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-08-31 22:05:37 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 2.59 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
+++ 프로그래머스/lv3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
@@ -1,48 +1,17 @@
 class Solution {
     public int solution(int n, int[] cores) {
-        int answer = 0;
-        int min = 0;
-        int max = n * cores[0];
-        int remain = 0;
-        int time = 0;
-
-        while (min <= max) {
-            int mid = (min + max) / 2;
-            int workCount = getCompletedWork(mid, cores);
-
-            if (workCount >= n) {
-                max = mid - 1;
-                time = mid;
-                remain = workCount;
-            } else {
-                min = mid + 1;
-            }
-        }
-
-        remain -= n;
-        for (int i = cores.length - 1; i >= 0; i--) {
-            if (time % cores[i] == 0) {
-                if (remain == 0) {
-                    answer = i + 1;
-                    break;
+        int cnt = 0;
+        while (true) {
+            for (int i = 0; i < cores.length; i++) {
+                if (cnt % cores[i] == 0) {
+                    n--;
+                    if (n == 0) {
+                        return i + 1;
+                    }
                 }
-                remain--;
             }
+            cnt++;
         }
 
-        return answer;
-    }
-
-    private int getCompletedWork(int time, int[] cores) {
-        if (time == 0) {
-            return cores.length;
-        }
-
-        int result = cores.length;
-        for (int i = 0; i < cores.length; i++) {
-            result += (time / cores[i]);
-        }
-
-        return result;
     }
 }
