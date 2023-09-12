773f32b0741b95d2cc88b18f76e26227b68663b3
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-09-12 22:28:54 +0900

    [level 3] Title: 표 편집, Time: 1126.07 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/81303. 표 편집/표 편집.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class Solution {
+    public String solution(int n, int k, String[] cmd) {
+        int maxLen = n;
+        Stack<Integer> delStack = new Stack<>();
+        for (String e : cmd) {
+            String command = e.split(" ")[0];
+            switch (command) {
+                case "U" -> k = k - Integer.parseInt(e.split(" ")[1]);
+                case "D" -> k = k + Integer.parseInt(e.split(" ")[1]);
+                case "C" -> {
+                    delStack.push(k);
+                    maxLen--;
+                    if (k == maxLen) {
+                        k--;
+                    }
+                }
+                case "Z" -> {
+                    int i = delStack.pop();
+                    maxLen++;
+                    if (k >= i) {
+                        k++;
+                    }
+                }
+            }
+        }
+
+        StringBuilder answer = new StringBuilder("O".repeat(maxLen));
+        while (!delStack.isEmpty()) {
+            answer.insert(delStack.pop().intValue(), 'X');
+        }
+
+        return answer.toString();
+    }
+}
