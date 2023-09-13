0e3530b2673d668c5cc3e17ab92e0690e9d06da0
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-09-13 22:36:56 +0900

    [level 3] Title: 거스름돈, Time: 18.04 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/lv3/12907. 거스름돈/거스름돈.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int n, int[] money) {
         int[] cache = new int[n + 1];
         cache[0] = 1;
-        for (int e : money) {
-            for (int i = e; i <= n; i++) {
-                cache[i] += cache[i - e];
+        for (int coin : money) {
+            for (int i = coin; i <= n; i++) {
+                cache[i] += cache[i - coin];
                 cache[i] %= 1_000_000_007;
             }
         }
