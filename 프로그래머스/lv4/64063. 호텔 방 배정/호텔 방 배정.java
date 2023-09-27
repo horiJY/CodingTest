c2f3d3d6895078826cae7f580a9b6c246484f861
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-09-27 13:53:20 +0900

    [level 4] Title: 호텔 방 배정, Time: 764.61 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -13,21 +13,16 @@ class Solution {
         for (int i = 0; i < room_number.length; i++) {
             long targetNum = room_number[i];
 
-            while (true) {
-                if (nextRoom.get(targetNum) == null) {
-                    answer[i] = targetNum;
-                    nextRoom.put(targetNum, targetNum + 1);
+            while (nextRoom.get(targetNum) != null) {
+                refreshRoom.offer(targetNum);
+                targetNum = nextRoom.get(targetNum);
+            }
 
-                    if (!refreshRoom.isEmpty()) {
-                        while (!refreshRoom.isEmpty()) {
-                            nextRoom.put(refreshRoom.poll(), targetNum + 1);
-                        }
-                    }
-                    break;
-                } else {
-                    refreshRoom.offer(targetNum);
-                    targetNum = nextRoom.get(targetNum);
-                }
+            answer[i] = targetNum;
+            nextRoom.put(targetNum, targetNum + 1);
+
+            while (!refreshRoom.isEmpty()) {
+                nextRoom.put(refreshRoom.poll(), targetNum + 1);
             }
         }
 
