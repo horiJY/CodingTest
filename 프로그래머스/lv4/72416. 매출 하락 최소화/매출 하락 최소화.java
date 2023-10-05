5866621569cc15c555267d3b4456487c8b3b0c49
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-10-06 04:32:10 +0900

    [level 4] Title: 매출 하락 최소화, Time: 0.05 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/72416. 매출 하락 최소화/매출 하락 최소화.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+
+    Map<Integer, List<Integer>> memberMap;
+    int[][] cache;
+    int[] cost;
+
+    public int solution(int[] sales, int[][] links) {
+        memberMap = new HashMap<>();
+        for (int[] is : links) {
+            List<Integer> ml = memberMap.get(is[0]);
+            if (ml == null) {
+                ml = new ArrayList<Integer>();
+            }
+
+            ml.add(is[1]);
+            memberMap.put(is[0], ml);
+        }
+
+        cost = sales;
+        // cache[미참][참여]
+        cache = new int[sales.length + 1][2];
+        DFS(1);
+
+        int answer = Math.min(cache[1][0], cache[1][1]);
+        return answer;
+    }
+
+    private void DFS(int num) {
+        List<Integer> cl = memberMap.get(num);
+        cache[num][1] = cost[num - 1];
+
+        if (cl == null) {
+            return;
+        }
+
+        int childCost = Integer.MAX_VALUE;
+        for (Integer child : cl) {
+            DFS(child);
+
+            if (cache[child][0] < cache[child][1]) {
+                // 자식 미참여
+                cache[num][0] += cache[child][0];
+                cache[num][1] += cache[child][0];
+                childCost = Math.min(childCost, cache[child][1] - cache[child][0]);
+            } else {
+                // 자식 참여
+                cache[num][0] += cache[child][1];
+                cache[num][1] += cache[child][1];
+                childCost = 0;
+            }
+        }
+
+        cache[num][0] += childCost;
+    }
+}
