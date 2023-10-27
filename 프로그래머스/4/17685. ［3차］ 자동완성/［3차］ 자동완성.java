a8a2c741ac825de90e55619ad0b4c3d220db6e37
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-10-27 18:03:05 +0900

    [level 4] Title: [3차] 자동완성, Time: 0.27 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
@@ -1,68 +1,40 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
 
     public int solution(String[] words) {
-        Tree tree = new Tree();
+        int answer = 0;
+        Arrays.sort(words);
+
         for (int i = 0; i < words.length; i++) {
-            tree.insert(words[i]);
+            int len = 0;
+            if (i > 0) {
+                len = compare(words[i - 1], words[i]);
+            }
+
+            if (i < words.length - 1) {
+                len = Math.max(len, compare(words[i], words[i + 1]));
+            }
+
+            if (len != words[i].length()) {
+                len++;
+            }
+
+            answer += len;
         }
 
-        int result = 0;
-        for (int i = 0; i < words.length; i++) {
-            result += tree.getMinInputCnt(words[i]);
-        }
-
-        return result;
+        return answer;
     }
 
-    private class Node {
-        Map<Character, Node> child = new HashMap<>();
-        Node parent = null;
-        Boolean isWordEnd = false;
+    private int compare(String s1, String s2) {
+        int len = 0;
+        for (int i = 0; i < Math.min(s1.length(), s2.length()); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                break;
+            }
+            len++;
+        }
+        return len;
     }
 
-    private class Tree {
-        final Node root = new Node();
-
-        public void insert(String word) {
-            Node cur = this.root;
-
-            for (int i = 0; i < word.length(); i++) {
-                Node parent = cur;
-                cur = cur.child.computeIfAbsent(word.charAt(i), k -> new Node());
-                cur.parent = parent;
-            }
-
-            cur.isWordEnd = true;
-        }
-
-        public int getMinInputCnt(String word) {
-            Node cur = this.root;
-            for (int i = 0; i < word.length(); i++) {
-                cur = cur.child.get(word.charAt(i));
-            }
-
-            // 마지막 단어가 끝이 X -> 전체 입력
-            if (cur.child.size() != 0) {
-                return word.length();
-            }
-
-            // 처음 단어가 아니면서
-            // 다른 완성 단어 입력된 곳 or 두번이상 입력된 곳 찾기
-            int cnt = 0;
-            while (cur != null) {
-                cur = cur.parent;
-
-                if (cur != null && !cur.isWordEnd && cur.child.size() == 1) {
-                    cnt++;
-                } else {
-                    break;
-                }
-            }
-
-            return word.length() - cnt;
-        }
-    }
 }
