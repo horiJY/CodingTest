434a9fe3a26c39959217c0d2556028fa8a73861d
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-10-30 21:39:29 +0900

    [level 2] Title: H-Index, Time: 1.36 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length; i++) {
+            answer = Math.max(answer, Math.min(citations[i], citations.length - i));
+        }
+        return answer;
+    }
+}
