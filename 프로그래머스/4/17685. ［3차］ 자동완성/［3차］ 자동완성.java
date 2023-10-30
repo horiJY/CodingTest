0f1ef082fb268d158d0497affbae2adc8073d1f6
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-10-30 23:06:03 +0900

    [level 4] Title: [3차] 자동완성, Time: 0.30 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
@@ -8,6 +8,7 @@ class Solution {
 
         for (int i = 0; i < words.length; i++) {
             int len = 0;
+            
             if (i > 0) {
                 len = compare(words[i - 1], words[i]);
             }
@@ -28,12 +29,14 @@ class Solution {
 
     private int compare(String s1, String s2) {
         int len = 0;
+        
         for (int i = 0; i < Math.min(s1.length(), s2.length()); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
                 break;
             }
             len++;
         }
+        
         return len;
     }
 
