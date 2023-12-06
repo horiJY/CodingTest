91d74c11f87f5aaffb788dbeea3fdba3a0a7bf02
Jaeyoung <45135200+horiJY@users.noreply.github.com>
2023-12-07 03:09:41 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 132 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,33 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] sequence, cache;
+    static int N;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(br.readLine());
-        int[] sequence = new int[size];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < sequence.length; i++) {
+        N = Integer.parseInt(br.readLine());
+        sequence = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
             sequence[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] moreCount = new int[size];
-        for (int i = 0; i < sequence.length; i++) {
-            moreCount[i] = 1;
-            for (int j = 0; j <= i; j++) {
-                if (sequence[j] < sequence[i] && moreCount[i] < moreCount[j] + 1) {
-                    moreCount[i] = moreCount[j] + 1;
-                }
+        cache = new int[N];
+        Arrays.fill(cache, Integer.MAX_VALUE);
+        cache[0] = sequence[0];
+        for (int i = 0, cIdx = 0; i < N; i++) {
+            if (cache[cIdx] < sequence[i]) {
+                cache[++cIdx] = sequence[i];
+            } else {
+                int fixIdx = findIdx(0, cIdx, sequence[i]);
+                cache[fixIdx] = sequence[i];
             }
         }
 
-        int answer = 1;
-        for (int count : moreCount) {
-            answer = answer < count ? count : answer;
+        for (int i = N - 1; i >= 0; i--) {
+            if (cache[i] != Integer.MAX_VALUE) {
+                System.out.println(i + 1);
+                break;
+            }
         }
-        System.out.println(answer);
+    }
+
+    private static int findIdx(int lo, int hi, int target) {
+        if(lo == hi){
+            return lo;
+        }
+        
+        if(lo+1 == hi){
+            return cache[lo] >= target ? lo : hi;
+        }
+        
+        int mid = (lo + hi) / 2;
+        if(cache[mid] == target){
+            return mid;
+        }
+        if(cache[mid] < target){
+            return findIdx(mid+1, hi, target);
+        }else{
+            return findIdx(lo, mid, target);
+        }
     }
 }
